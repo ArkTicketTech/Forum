f90a4019a043247bdf5bae8984757cf54a7c4da3
wfnuser <wfnuser@126.com>
2017-04-17 17:00:19 +0800

    [INIT]

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'flarum',
+    'username' => 'root',
+    'password' => 'ArkTicketTech2017',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => '',
+    'strict' => false,
+  ),
+  'url' => 'http://arkticket.tech',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
